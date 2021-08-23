a5b5ef1f42876591037a063b2cfc720bfe9f95d7
Gal Horovits <galhorovits@Gals-MBP.lan>
2021-08-23 11:53:52 -0400

    first step of npm package - importMetrics  and reportwebvital functions

---
+++ index.js
@@ -0,0 +1,32 @@
+import nc from "next-connect";
+import fs from "fs";
+
+export const importMetrics = (req, res) => {
+  console.log("succesfully entered insideHandler");
+  // console.log("req", req.body);
+  const json = JSON.parse(req.body);
+  const { name, value } = json;
+
+  if (!fs.existsSync("metrics.json")) {
+    const newObj = { metrics: [{}] };
+    fs.writeFileSync("metrics.json", JSON.stringify(newObj, null, 4));
+  }
+
+  const results = JSON.parse(
+    fs.readFileSync("metrics.json", "utf-8", (err, data) => {
+      console.log(data);
+    })
+  );
+  results.metrics[0][name] = value; //.toFixed(2);
+
+  fs.writeFileSync("metrics.json", JSON.stringify(results, null, 4));
+  res.status(200).json({ test: "test worked" });
+};
+
+export async function reportWebVitals(metric) {
+  const body = JSON.stringify(metric);
+  const url = "/api";
+
+  console.log("BODY IS", body);
+  await fetch(url, { body, method: "POST", keepalive: true });
+}
