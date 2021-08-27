e5ea414940658e9a7e04690f0aa44c9493cefc84
Kelly Yeo <kellyyeo@Kellys-MacBook-Air.local>
2021-08-27 12:24:40 -0400

    oops

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ export const importMetrics = (req, res) => {
   const json = JSON.parse(req.body);
   const { name, value } = json;
 
+  // if metrics.json file doesn't exist, create it. 
   if (!fs.existsSync("metrics.json")) {
     const newObj = { metrics: [{}] };
     fs.writeFileSync("metrics.json", JSON.stringify(newObj, null, 4));
   }
 
-  const results = JSON.parse(
+  // parse the metrics.json file and store in results
+  const metricsData = JSON.parse(
     fs.readFileSync("metrics.json", "utf-8", (err, data) => {
-      console.log(data);
+      console.log("metrics calculated", data);
     })
   );
-  results.metrics[0][name] = value; //.toFixed(2);
+  console.log('metricsData from file',metricsData);
+  metricsData.metrics[0][name] = value; //.toFixed(2);
 
+  
   fs.writeFileSync("metrics.json", JSON.stringify(results, null, 4));
   res.status(200).json({ test: "test worked" });
 };
