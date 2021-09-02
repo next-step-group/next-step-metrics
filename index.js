1a71a12d5f25a7d23b384a6d86a2b6b18039a579
Kelly Yeo <kellyyeo@Kellys-Air.fios-router.home>
2021-09-02 16:19:49 -0400

    merge

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import nc from "next-connect";
 import fs from "fs";
 
 export const importMetrics = (req, res) => {
@@ -7,29 +6,25 @@ export const importMetrics = (req, res) => {
   const json = JSON.parse(req.body);
   const { name, value } = json;
 
-  // if metrics.json file doesn't exist, create it. 
-  if (!fs.existsSync("metrics.json")) {
+  if (!fs.existsSync("NextStepMetrics.json")) {
     const newObj = { metrics: [{}] };
-    fs.writeFileSync("metrics.json", JSON.stringify(newObj, null, 4));
+    fs.writeFileSync("NextStepMetrics.json", JSON.stringify(newObj, null, 4));
   }
 
-  // parse the metrics.json file and store in results
-  const metricsData = JSON.parse(
-    fs.readFileSync("metrics.json", "utf-8", (err, data) => {
-      console.log("metrics calculated", data);
+  const results = JSON.parse(
+    fs.readFileSync("NextStepMetrics.json", "utf-8", (err, data) => {
+      console.log(data);
     })
   );
-  console.log('metricsData from file',metricsData);
-  metricsData.metrics[0][name] = value; //.toFixed(2);
+  results.metrics[0][name] = value; //.toFixed(2);
 
-  
-  fs.writeFileSync("metrics.json", JSON.stringify(results, null, 4));
+  fs.writeFileSync("NextStepMetrics.json", JSON.stringify(results, null, 4));
   res.status(200).json({ test: "test worked" });
 };
 
 export async function reportWebVitals(metric) {
   const body = JSON.stringify(metric);
-  const url = "/api/NSMetrics";
+  const url = "/api/NextStepMetrics";
 
   console.log("BODY IS", body);
   await fetch(url, { body, method: "POST", keepalive: true });
