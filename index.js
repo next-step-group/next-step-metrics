d9ed984cc73d72d4566148e1f49e4e20d6ae14fa
Kelly Yeo <kellyyeo@Kellys-Air.fios-router.home>
2021-09-06 15:34:34 -0400

    merge for new branch

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const fs = require('fs')
 
 // Handles Next.js backend metrics calls
-exports.importMetrics = (req, res) => {
+exports.metrics = (req, res) => {
   const json = JSON.parse(req.body);
   const { name , value } = json;
+  const filePath = "./node_modules/next-step-metrics/next-step-metrics.json";
 
   // Generate a metrics.json file in your project's root folder to house web vitals data
-  if (!fs.existsSync("NextStepMetrics.json")) {
+  if (!fs.existsSync(filePath)) {
     const newObj = { metrics: {}, logs: [], id : 0};
-    fs.writeFileSync("NextStepMetrics.json", JSON.stringify(newObj, null, 4));
+    fs.writeFileSync(filePath, JSON.stringify(newObj, null, 4));
   }
 
   // Format and add data to the metrics.json file
-  const results = JSON.parse(fs.readFileSync("NextStepMetrics.json", "utf-8"));
+  const results = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
   if (results.metrics.FCP) {
     if (name === "Next.js-hydration") {
@@ -26,16 +27,18 @@ exports.importMetrics = (req, res) => {
   results.metrics[name] = value;
   results.metrics.date = new Date();
 
-  fs.writeFileSync("NextStepMetrics.json", JSON.stringify(results, null, 4));
+  fs.writeFileSync(filePath, JSON.stringify(results, null, 4));
 
   res.status(200).json({ test: "test worked" });
 };
 
-// Sends Metrics data to /NextStepMetrics on page load
+// Sends Metrics data to /next-step-metrics on page load
 exports.reportWebVitals = async (metric) => {
   const body = JSON.stringify(metric);
-  const url = "/api/NextStepMetrics";
-
+  const url = "/api/next-step";
+  console.log('BODY IS: ', body);
+  
   await fetch(url, { body, method: "POST", keepalive: true });
+  
 }
 
