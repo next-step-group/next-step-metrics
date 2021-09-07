ec156efdd19279d29d530dd8ad6696fc3a224e02
SYu449 <sy449@drexel.edu>
2021-09-07 16:13:32 -0400

    No longer wipes metrics

--- index.js
+++ index.js
@@ -8,35 +8,30 @@ exports.metrics = (req, res) => {
 
   // Generate a metrics.json file in your project's root folder to house web vitals data
   if (!fs.existsSync(filePath)) {
-    const newObj = { metrics: {}, logs: [], id : 0};
+    const newObj = { metrics: {}, logs: [] };
     fs.writeFileSync(filePath, JSON.stringify(newObj, null, 4));
   }
 
   // Format and add data to the metrics.json file
   const results = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-  if (results.metrics.FCP) {
-    if (name === "Next.js-hydration") {
-      results.metrics.id = results.id;
-      results.logs.push(results.metrics);
-      results.metrics = {};
-      results.id++;
-    }
+  if (name === "TTFB") {
+    results.metrics.date = new Date();
+    results.logs.push(results.metrics);
   }
-
+  
   results.metrics[name] = value;
-  results.metrics.date = new Date();
 
   fs.writeFileSync(filePath, JSON.stringify(results, null, 4));
 
   res.status(200).json({ test: "test worked" });
 };
 
-// Sends Metrics data to /next-step-metrics on page load
+// Sends Metrics data to /NextStepMetrics on page load
 exports.reportWebVitals = async (metric) => {
   const body = JSON.stringify(metric);
   const url = "/api/next-step";
+
   await fetch(url, { body, method: "POST", keepalive: true });
-  
 }
 
